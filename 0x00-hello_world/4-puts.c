b782d5f521c41ca5d6c2a38b9fae065f4acc935f
nuelokim <okime784@gmail.com>
2023-07-13 14:59:56 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - ENTRY POINT 
+ *
+ * Return: Always 0 (Process)
+ */
+int main(void)
+{
+ 	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
