033b84e2eaa9863b72ed4295b892826599ad1194
nuelokim <okime784@gmail.com>
2023-07-24 03:05:14 -0700

    5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char s[10] = "My School";
+
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
+	return (0);
+}
