d7c658f11211643f7d3e2fd4f2c3a46268cd3728
nuelokim <okime784@gmail.com>
2023-07-26 08:18:36 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	char s[] = "Look up!\n";
+	char *p;
+
+	p = string_toupper(s);
+	printf("%s", p);
+	printf("%s", s);
+	return (0);
+}
